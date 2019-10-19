b7d4a2b80800a782f0d951942c31ae45e8847873
Zoe <zoekharvey@gmail.com>
2019-10-19 14:20:46 -0600

    Start setting up files

---
+++ src/WelcomePage/WelcomePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './WelcomePage.css'
+
+const WelcomePage = () => {
+
+  return(
+    <section className="section__welcome--page">
+      <h1 className="typewriter">
+        Welcome to Niko's Page
+      </h1>
+    </section>
+  )
+}
+
+export default WelcomePage;
+
